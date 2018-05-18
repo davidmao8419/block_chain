f89f8b5f0df327e5af355bf5e8c0bfd68fd6a592
davidmao8419 <davidmao8419@gmail.com>
2018-05-18 15:25:46 +0800

    add mine block and test

--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,9 @@
 const Block = require('./block');
 
+const booBlock = Block.mineBlock(Block.genesis(), "boo");
+console.log(booBlock.toString());
+/*
 const block = new Block('boo', 'bar', 'zoo', 'baz');
 console.log(block.toString());
 console.log(Block.genesis().toString());
+*/
